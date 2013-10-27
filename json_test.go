4c1b03581ac4fbcc99818dfe2fc1a8e016eded33
Craig Jackson <tapocol@gmail.com>
2013-10-27 11:56:52 -0700

    Fix test for deprecated JSON funcs.

--- json_test.go
+++ json_test.go
@@ -23,16 +23,8 @@ func TestJSON(t *testing.T) {
 	expect.A = 1
 	expect.B = "hello"
 
-	if err := WriteJSON(wc, &expect); err != nil {
-		t.Fatal("write", err)
-	}
-
 	if err := wc.WriteJSON(&expect); err != nil {
 		t.Fatal("write", err)
-	}
-
-	if err := ReadJSON(rc, &expect); err != nil {
-		t.Fatal("read", err)
 	}
 
 	if err := rc.ReadJSON(&actual); err != nil {
@@ -43,3 +35,30 @@ func TestJSON(t *testing.T) {
 		t.Fatal("equal", actual, expect)
 	}
 }
+
+func TestDeprecatedJSON(t *testing.T) {
+	var buf bytes.Buffer
+	c := fakeNetConn{&buf, &buf}
+	wc := newConn(c, true, 1024, 1024)
+	rc := newConn(c, false, 1024, 1024)
+
+	var actual, expect struct {
+		A int
+		B string
+	}
+	expect.A = 1
+	expect.B = "hello"
+
+	if err := WriteJSON(wc, &expect); err != nil {
+		t.Fatal("write", err)
+	}
+
+	if err := ReadJSON(rc, &actual); err != nil {
+		t.Fatal("read", err)
+	}
+
+	if !reflect.DeepEqual(&actual, &expect) {
+		t.Fatal("equal", actual, expect)
+	}
+}
+
