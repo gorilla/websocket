b2f990180a8e1fc85b4e9234160bc962bfcaa15a
Joachim Bauch <bauch@struktur.de>
2014-04-16 17:25:17 +0200

    Implement Upgrader API (#6).

--- server.go
+++ server.go
@@ -5,9 +5,6 @@
 package websocket
 
 import (
-	"bufio"
-	"errors"
-	"net"
 	"net/http"
 	"strings"
 )
@@ -19,6 +16,8 @@ type HandshakeError struct {
 
 func (e HandshakeError) Error() string { return e.message }
 
+// This method is deprecated, use websocket.Upgrader instead.
+//
 // Upgrade upgrades the HTTP server connection to the WebSocket protocol.
 //
 // The application is responsible for checking the request origin before
@@ -47,77 +46,11 @@ func (e HandshakeError) Error() string { return e.message }
 // error of type HandshakeError. Applications should handle this error by
 // replying to the client with an HTTP error response.
 func Upgrade(w http.ResponseWriter, r *http.Request, responseHeader http.Header, readBufSize, writeBufSize int) (*Conn, error) {
-
-	if values := r.Header["Sec-Websocket-Version"]; len(values) == 0 || values[0] != "13" {
-		return nil, HandshakeError{"websocket: version != 13"}
+	u := Upgrader{ReadBufferSize: readBufSize, WriteBufferSize: writeBufSize}
+	u.Error = func(w http.ResponseWriter, r *http.Request, status int, reason error) {
+		// don't return errors to maintain backwards compatibility
 	}
-
-	if !tokenListContainsValue(r.Header, "Connection", "upgrade") {
-		return nil, HandshakeError{"websocket: connection header != upgrade"}
-	}
-
-	if !tokenListContainsValue(r.Header, "Upgrade", "websocket") {
-		return nil, HandshakeError{"websocket: upgrade != websocket"}
-	}
-
-	var challengeKey string
-	values := r.Header["Sec-Websocket-Key"]
-	if len(values) == 0 || values[0] == "" {
-		return nil, HandshakeError{"websocket: key missing or blank"}
-	}
-	challengeKey = values[0]
-
-	var (
-		netConn net.Conn
-		br      *bufio.Reader
-		err     error
-	)
-
-	h, ok := w.(http.Hijacker)
-	if !ok {
-		return nil, errors.New("websocket: response does not implement http.Hijacker")
-	}
-	var rw *bufio.ReadWriter
-	netConn, rw, err = h.Hijack()
-	br = rw.Reader
-
-	if br.Buffered() > 0 {
-		netConn.Close()
-		return nil, errors.New("websocket: client sent data before handshake is complete")
-	}
-
-	c := newConn(netConn, true, readBufSize, writeBufSize)
-	if responseHeader != nil {
-		c.subprotocol = responseHeader.Get("Sec-Websocket-Protocol")
-	}
-
-	p := c.writeBuf[:0]
-	p = append(p, "HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: "...)
-	p = append(p, computeAcceptKey(challengeKey)...)
-	p = append(p, "\r\n"...)
-	for k, vs := range responseHeader {
-		for _, v := range vs {
-			p = append(p, k...)
-			p = append(p, ": "...)
-			for i := 0; i < len(v); i++ {
-				b := v[i]
-				if b <= 31 {
-					// prevent response splitting.
-					b = ' '
-				}
-				p = append(p, b)
-			}
-			p = append(p, "\r\n"...)
-		}
-	}
-	p = append(p, "\r\n"...)
-
-	if _, err = netConn.Write(p); err != nil {
-		netConn.Close()
-		return nil, err
-	}
-
-	return c, nil
+	return u.Upgrade(w, r, responseHeader)
 }
 
 // Subprotocols returns the subprotocols requested by the client in the
