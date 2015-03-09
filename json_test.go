2dbddebb8266b93c5e6b119efb54e89043186f3f
Gary Burd <gary@beagledreams.com>
2015-03-09 12:10:02 -0700

    Improve the errors returned from ReadJSON.
    The JSON decoder returns io.EOF when a message is empty or all
    whitespace. Convert io.EOF return values from the JSON decoder to
    io.ErrUnexpectedEOF so that applications can distinguish between an
    error reading the JSON value and the connection closing.

--- json_test.go
+++ json_test.go
@@ -6,6 +6,8 @@ package websocket
 
 import (
 	"bytes"
+	"encoding/json"
+	"io"
 	"reflect"
 	"testing"
 )
@@ -33,6 +35,60 @@ func TestJSON(t *testing.T) {
 
 	if !reflect.DeepEqual(&actual, &expect) {
 		t.Fatal("equal", actual, expect)
+	}
+}
+
+func TestPartialJsonRead(t *testing.T) {
+	var buf bytes.Buffer
+	c := fakeNetConn{&buf, &buf}
+	wc := newConn(c, true, 1024, 1024)
+	rc := newConn(c, false, 1024, 1024)
+
+	var v struct {
+		A int
+		B string
+	}
+	v.A = 1
+	v.B = "hello"
+
+	messageCount := 0
+
+	// Partial JSON values.
+
+	data, err := json.Marshal(v)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for i := len(data) - 1; i >= 0; i-- {
+		if err := wc.WriteMessage(TextMessage, data[:i]); err != nil {
+			t.Fatal(err)
+		}
+		messageCount++
+	}
+
+	// Whitespace.
+
+	if err := wc.WriteMessage(TextMessage, []byte(" ")); err != nil {
+		t.Fatal(err)
+	}
+	messageCount++
+
+	// Close.
+
+	if err := wc.WriteMessage(CloseMessage, FormatCloseMessage(CloseNormalClosure, "")); err != nil {
+		t.Fatal(err)
+	}
+
+	for i := 0; i < messageCount; i++ {
+		err := rc.ReadJSON(&v)
+		if err != io.ErrUnexpectedEOF {
+			t.Error("read", i, err)
+		}
+	}
+
+	err = rc.ReadJSON(&v)
+	if err != io.EOF {
+		t.Error("final", err)
 	}
 }
 
