13e4d0621caa4d77fd9aa470ef6d7ab63d1a5e41
Gary Burd <gary@beagledreams.com>
2015-09-23 15:29:30 -0700

    Do not build files in echo example

--- examples/echo/server.go
+++ examples/echo/server.go
@@ -1,6 +1,8 @@
-// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
+// Copyright 2015 The Gorilla WebSocket Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+
+// +build ignore
 
 package main
 
