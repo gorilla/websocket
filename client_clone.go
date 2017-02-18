e1100da3009789e32d894dff8ded0202d2ec0fde
William Wennerström <william@willeponken.me>
2017-02-18 14:57:28 +0100

    Move build constraint to correct position

--- client_clone.go
+++ client_clone.go
@@ -2,9 +2,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package websocket
-
 // +build go1.8
+
+package websocket
 
 import "crypto/tls"
 
