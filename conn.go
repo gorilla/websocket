e9a52af44b4e7d9da189abc18177757e2114b03d
misu <misu@aptpod.co.jp>
2018-02-09 10:13:43 +0900

    mod: unnecessary format change

--- conn.go
+++ conn.go
@@ -83,14 +83,20 @@ const (
 	PongMessage = 10
 )
 
-type (
-	// netError satisfies the net Error interface.
-	netError struct {
-		msg       string
-		temporary bool
-		timeout   bool
-	}
-)
+// ErrCloseSent is returned when the application writes a message to the
+// connection after sending a close message.
+var ErrCloseSent = errors.New("websocket: close sent")
+
+// ErrReadLi	mit is returned when reading a message that is larger than the
+// read limit set for the connection.
+var ErrReadLimit = errors.New("websocket: read limit exceeded")
+
+// netError satisfies the net Error interface.
+type netError struct {
+	msg       string
+	temporary bool
+	timeout   bool
+}
 
 func (e *netError) Error() string   { return e.msg }
 func (e *netError) Temporary() bool { return e.temporary }
@@ -174,14 +180,6 @@ var (
 	errBadWriteOpCode      = errors.New("websocket: bad write message type")
 	errWriteClosed         = errors.New("websocket: write closed")
 	errInvalidControlFrame = errors.New("websocket: invalid control frame")
-
-	// ErrCloseSent is returned when the application writes a message to the
-	// connection after sending a close message.
-	ErrCloseSent = errors.New("websocket: close sent")
-
-	// ErrReadLimit is returned when reading a message that is larger than the
-	// read limit set for the connection.
-	ErrReadLimit = errors.New("websocket: read limit exceeded")
 )
 
 func newMaskKey() [4]byte {
