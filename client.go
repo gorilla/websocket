d926999018d79949fca2047104472903c4b77c28
Chan Kang <chankang@chankang17@gmail.com>
2022-06-19 15:15:12 -0400

    wrapping the error in %w

--- client.go
+++ client.go
@@ -375,9 +375,9 @@ func (d *Dialer) DialContext(ctx context.Context, urlStr string, requestHeader h
 			for _, proto := range d.TLSClientConfig.NextProtos {
 				if proto != "http/1.1" {
 					return nil, nil, fmt.Errorf(
-						"websocket: protocol %q was given but is not supported; "+
-							"sharing tls.Config with net/http Transport can cause this error",
-						proto,
+						"websocket: protocol %q was given but is not supported;"+
+							"sharing tls.Config with net/http Transport can cause this error: %w",
+						proto, err,
 					)
 				}
 			}
