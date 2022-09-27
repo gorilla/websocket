7f54dba00e5f01f2742ea398429d95b768f67c57
xieyuschen <xieyuschen@gmail.com>
2022-09-27 14:05:55 +0800

    avoid flushing twice

--- json.go
+++ json.go
@@ -5,15 +5,15 @@
 package websocket
 
 import (
-	"encoding/json"
-	"io"
+    "encoding/json"
+    "io"
 )
 
 // WriteJSON writes the JSON encoding of v as a message.
 //
 // Deprecated: Use c.WriteJSON instead.
 func WriteJSON(c *Conn, v interface{}) error {
-	return c.WriteJSON(v)
+    return c.WriteJSON(v)
 }
 
 // WriteJSON writes the JSON encoding of v as a message.
@@ -21,16 +21,16 @@ func WriteJSON(c *Conn, v interface{}) error {
 // See the documentation for encoding/json Marshal for details about the
 // conversion of Go values to JSON.
 func (c *Conn) WriteJSON(v interface{}) error {
-	w, err := c.NextWriter(TextMessage)
-	if err != nil {
-		return err
-	}
-	err1 := json.NewEncoder(w).Encode(v)
-	err2 := w.Close()
-	if err1 != nil {
-		return err1
-	}
-	return err2
+    w, err := c.NextWriter(TextMessage)
+    if err != nil {
+        return err
+    }
+    
+    if err = json.NewEncoder(w).Encode(v); err != nil {
+        return err
+    }
+    
+    return w.Close()
 }
 
 // ReadJSON reads the next JSON-encoded message from the connection and stores
@@ -38,7 +38,7 @@ func (c *Conn) WriteJSON(v interface{}) error {
 //
 // Deprecated: Use c.ReadJSON instead.
 func ReadJSON(c *Conn, v interface{}) error {
-	return c.ReadJSON(v)
+    return c.ReadJSON(v)
 }
 
 // ReadJSON reads the next JSON-encoded message from the connection and stores
@@ -47,14 +47,14 @@ func ReadJSON(c *Conn, v interface{}) error {
 // See the documentation for the encoding/json Unmarshal function for details
 // about the conversion of JSON to a Go value.
 func (c *Conn) ReadJSON(v interface{}) error {
-	_, r, err := c.NextReader()
-	if err != nil {
-		return err
-	}
-	err = json.NewDecoder(r).Decode(v)
-	if err == io.EOF {
-		// One value is expected in the message.
-		err = io.ErrUnexpectedEOF
-	}
-	return err
+    _, r, err := c.NextReader()
+    if err != nil {
+        return err
+    }
+    err = json.NewDecoder(r).Decode(v)
+    if err == io.EOF {
+        // One value is expected in the message.
+        err = io.ErrUnexpectedEOF
+    }
+    return err
 }
