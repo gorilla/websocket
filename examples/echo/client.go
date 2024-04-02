4ab616eadcd61fe0d7fcac73785c99aff3d1029e
Barry Yan <62547564+ibarryyan@users.noreply.github.com>
2024-04-02 20:28:05 +0800

    remove client.go mt type

--- examples/echo/client.go
+++ examples/echo/client.go
@@ -41,12 +41,12 @@ func main() {
 	go func() {
 		defer close(done)
 		for {
-			mt, message, err := c.ReadMessage()
+			_ , message, err := c.ReadMessage()
 			if err != nil {
 				log.Println("read:", err)
 				return
 			}
-			log.Printf("recv: %s, type: %s", message, websocket.FormatMessageType(mt))
+			log.Printf("recv: %s", message)
 		}
 	}()
 
